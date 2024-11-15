8255d61e3084a379dfa7f67a6a0f7e3a53ce5d79
Juan G. Carmona <juan@jgcarmona.com>
2024-11-15 11:35:29 +0100

    Fix tf formats

--- infrastructure/modules/app_services/api/main.tf
+++ infrastructure/modules/app_services/api/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_linux_web_app" "api" {
   location            = var.location
   resource_group_name = var.resource_group_name
   service_plan_id     = var.service_plan_id
-  https_only = true
+  https_only          = true
   site_config {
     application_stack {
       dotnet_version = "8.0" # Use application_stack to specify the .NET version
