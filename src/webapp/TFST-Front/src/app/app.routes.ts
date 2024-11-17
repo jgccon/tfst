add25ce635da59a1d455ce71a4aed9010d053a92
Alvaro <aaleyva97@gmail.com>
2024-11-17 11:17:30 -0800

    add sakai

--- src/webapp/TFST-Front/src/app/app.routes.ts
+++ src/webapp/TFST-Front/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AppLayoutComponent } from './layout/app.layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        component: AppLayoutComponent,
+
+    }
+    
+];
