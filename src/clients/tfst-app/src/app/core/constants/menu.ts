3642ff68c9cf62fdda4cea2f84727e630523e5ef
aaleyva97 <109109907+aaleyva97@users.noreply.github.com>
2025-05-15 21:33:14 -0700

    add-tfst app (#117)
    * add-tfst app
    
    * refactor: flatten project structure by moving TFST-Front into tfst-app and update config
    
    * docs: update README.md to reflect project purpose and key features
    
    ---------
    
    Co-authored-by: Juan G Carmona <juan@jgcarmona.com>

---
+++ src/clients/tfst-app/src/app/core/constants/menu.ts
@@ -0,0 +1,17 @@
+export const Menu = [
+    {
+        items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-chart-bar', routerLink: ['/'] }]
+    },
+    {
+        items: [{ label: 'Projects', icon: 'pi pi-fw pi-desktop', routerLink: ['/s'] }]
+    },
+    {
+        items: [{ label: 'Clients', icon: 'pi pi-fw pi-users', routerLink: ['/s'] }]
+    },
+    {
+        items: [{ label: 'Reports', icon: 'pi pi-fw pi-chart-line', routerLink: ['/s'] }]
+    },
+    {
+        items: [{ label: 'Configuration', icon: 'pi pi-fw pi-cog', routerLink: ['/s'] }]
+    },
+];
