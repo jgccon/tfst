af6e7b09081c0595f5f69690d00888f9a7e8ef28
Juan G Carmona <juan@jgcarmona.com>
2025-07-04 12:12:22 +0200

    Added sitemap and tailwindcss

--- src/clients/full-stack-team/astro.config.mjs
+++ src/clients/full-stack-team/astro.config.mjs
@@ -1,5 +1,29 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+import tailwindcss from '@tailwindcss/vite';
+
+
+export default defineConfig({
+  site: 'https://full-stack.team',
+
+  integrations: [
+        sitemap({
+            i18n: {
+                defaultLocale: 'en', // All urls that don't contain `es` or `fr` after `https://full-stack.team/` will be treated as default locale, i.e. `en`
+                locales: {
+                en: 'en-US', // The `defaultLocale` value must present in `locales` keys
+                es: 'es-ES',
+                fr: 'fr-CA',
+                },
+            },
+            xslURL: '/sitemap.xsl',
+            filenameBase: 'the-full-stack-team-sitemap'
+        }),
+    ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
